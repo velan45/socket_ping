48af1bffe0b1443d772216e9e7866b8931013335
vijay <velan45@gmail.com>
2020-04-08 02:17:21 +0530

    Changes made in client code for calculating the round trip time as well as between packets

--- Client.cpp
+++ Client.cpp
@@ -9,15 +9,21 @@
 #include <string.h> 
 #include<string>
 #define PORT 8080 
+#define MICROSECONDS 1000000 // 1 seconds
 using namespace std;
+double time_diff( const clock_t start, const clock_t end )
+{
+	double time_taken = double(end - start) / double(CLOCKS_PER_SEC); 
+    return time_taken;
+}
 int main(int argc, char const *argv[]) 
 { 
 	int sock = 0, valread; 
-	unsigned int microseconds = 1000;
+	
 	struct sockaddr_in serv_addr; 
 	char *hello = "Hello from client"; 
 	char buffer[1024] = {0}; 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) //sock_stream(TCP) can be changed to sock_dgram(UDP)
 	{ 
 		printf("\n Socket creation error \n"); 
 		return -1; 
@@ -38,34 +44,46 @@ int main(int argc, char const *argv[])
 		printf("\nConnection Failed \n"); 
 		return -1; 
 	} 
-	clock_t start, end;
+	const clock_t start_pac, end_pac;
+	start_pac = clock();
 	for (int i=0; i<=100; i++)
 	{
-		string val = to_string(i);
-		string final_str1 = " packet no:";
-		final_str1.append(val);
-		const char*final_str = final_str1.data(); 
-		
-    send(sock , final_str , strlen(final_str) , 0 ); 
-	start = clock();
-	cout << "packet_sent"<<endl;
+	// preparing data packet
+	string val = to_string(i);
+	string final_str1 = " packet no:";
+	final_str1.append(val);
+	const char*final_str = final_str1.data(); 
+	
+	// Packet is prepared and waiting to send.	
+	usleep(MICROSECONDS);
+	
+	//controller program starting counter	
+	const clock_t start_SR = clock();	
+	send(sock , final_str , strlen(final_str) , 0 ); 
+	cout << "packet_sent"<<final_str << endl;
+	
+	// Going to recieve the packet_sent	
 	valread = read( sock, buffer, 1024);
+	
+	// packet comparison	
 	if (*final_str != *buffer)
 	{
 		cout << "packets are not same"<<endl;
 		continue;
 	}
-	usleep(microseconds);
-	end = clock();
-	double time_taken = double(end - start) / double(CLOCKS_PER_SEC); 
-    cout << "Time taken by program is : " << fixed  
-         << time_taken << end; 
-    cout << " sec " << endl;
-   
-	}
-
-    return 0; 
- 
 	
+	//stop the clock
+	const clock_t end_SR = clock();
+	
+	// round trip time calculation	
+	cout << " Round trip time of " << i << "is :" << time_diff(start_SR, end_SR);
+	
+	//between packet
+	end_pac = clock();
+	cout << " Between Packet " << i << "and " << i+1 << " is :" << time_diff(start_pac, end_pac);
+	start_pac = end_pac; 
+    
+	}
+    return 0; 
 } 
 
